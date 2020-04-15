7d8f0876d9ee045b36a6f709a56a29b53c20ecbc
Rishav Chaudhary <rishavch2104@gmail.com>
2020-04-15 23:43:24 +0530

    refactored authentication

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,14 +1,15 @@
 import React, { useState, createContext } from "react";
-
+import Router from "next/router";
 import firebase from "./firebaseConfig/firebaseConnect";
 
 export const signInWithGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
 
-  firebase.auth
+  firebase
+    .auth()
     .signInWithPopup(provider)
     .then(result => {
-      console.log(result);
+      return result;
     })
     .catch(e => console.log(e.message));
 };
@@ -17,25 +18,34 @@ export const AuthContext = createContext([{}, () => {}]);
 
 export function AuthProvider(props) {
   const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const handleLoginChange = toggle => {
     setLoggedIn(toggle);
+    handleAuthenticatingChange(false);
+  };
+  const handleAuthenticatingChange = toggle => {
+    setIsAuthenticating(toggle);
   };
   React.useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged(user => {
       if (user) {
         console.log({ user });
-        handleLoginChange(true);
+        setLoggedIn(true);
+        Router.push("/homepage", "/homepage");
       } else {
         console.log("no user");
         handleLoginChange(false);
+        Router.push("/index", "/index");
       }
     });
     return () => {
       unsubscribe();
     };
-  });
+  }, []);
   return (
-    <AuthContext.Provider value={(isLoggedIn, handleLoginChange)}>
+    <AuthContext.Provider
+      value={(isLoggedIn, handleLoginChange, isAuthenticating)}
+    >
       {props.children}
     </AuthContext.Provider>
   );
---
+++ src/pages/homepage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const Homepage = () => {
+  return <Typography>HomePage!</Typography>;
+};
+
+export default Homepage;
